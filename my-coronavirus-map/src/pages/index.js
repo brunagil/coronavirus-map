ccb21a98fbe3719a3c88fbb9d075db822603c9ce
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2020-04-27 15:35:26 -0300

    add data to map and update style

--- my-coronavirus-map/src/pages/index.js
+++ my-coronavirus-map/src/pages/index.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-
-import Layout from 'components/Layout';
-import Container from 'components/Container';
-import Map from 'components/Map';
-
-import axios from 'axios';
-
+import React from "react";
+import Helmet from "react-helmet";
+import L from "leaflet";
+import axios from "axios";
+import Layout from "components/Layout";
+import Container from "components/Container";
+import Map from "components/Map";
 
 const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369
+  lat: 0,
+  lng: 0,
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
-
 const IndexPage = () => {
-
   /**
    * mapEffect
    * @description Fires a callback once the page renders
@@ -25,50 +21,90 @@ const IndexPage = () => {
    */
 
   async function mapEffect({ leafletElement: map } = {}) {
-
-    let response; 
+    let response;
 
     try {
-      response = await axios.get('https://corona.lmao.ninja/v2/countries');
-
-    } catch(err) {
-      console.log('erro', err);
+      response = await axios.get("https://corona.lmao.ninja/v2/countries");
+    } catch (err) {
+      console.log("erro", err);
       return;
     }
-    console.log('response', response)
-    const { data = [] } = response; 
+    console.log("response", response);
+
+    const { data = [] } = response;
     const hasData = Array.isArray(data) && data.length > 0;
 
-    if(!hasData) return;
+    if (!hasData) return;
 
     const geoJson = {
-      type: 'FeatureCollection',
+      type: "FeatureCollection",
       features: data.map((country = {}) => {
         const { countryInfo = {} } = country;
-        const { lat, log: lng } = countryInfo;
+        const { lat, long: lng } = countryInfo;
         return {
-          type: 'Feature',
+          type: "Feature",
           properties: {
             ...country,
           },
           geometry: {
-            type: 'Point',
-            coordinates: [ lng, lat ]
-          }
+            type: "Point",
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    const geoJsonLayers = new L.GeoJSON(geoJson, {
+      pointToLayer: (feature = {}, latlng) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if (cases > 1000) {
+          casesString = `${casesString.slice(0, -3)}k+`;
         }
-      })
-    }
 
-    console.log(geoJson, 'geo data json')
+        if (updated) {
+          updatedFormatted = new Date(updated).toLocaleString();
+        }
 
+        const html = `
+        <span class="icon-marker">
+          <span class="icon-marker-tooltip">
+            <h2>${country}</h2>
+            <ul>
+              <li><strong>Confirmed:</strong> ${cases}</li>
+              <li><strong>Deaths:</strong> ${deaths}</li>
+              <li><strong>Recovered:</strong> ${recovered}</li>
+              <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+            </ul>
+          </span>
+          ${casesString}
+        </span>
+      `;
 
+        return L.marker(latlng, {
+          icon: L.divIcon({
+            className: "icon",
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo(map);
   }
 
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: 'OpenStreetMap',
+    defaultBaseMap: "OpenStreetMap",
     zoom: DEFAULT_ZOOM,
-    mapEffect
+    mapEffect,
   };
 
   return (
@@ -83,9 +119,14 @@ const IndexPage = () => {
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
         <pre>
-          <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-leaflet</code>
+          <code>
+            gatsby new [directory]
+            https://github.com/colbyfayock/gatsby-starter-leaflet
+          </code>
         </pre>
-        <p className="note">Note: Gatsby CLI required globally for the above command</p>
+        <p className="note">
+          Note: Gatsby CLI required globally for the above command
+        </p>
       </Container>
     </Layout>
   );
